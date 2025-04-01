b74a35f2df69c7acfcc9bab7acaaaeda1b76aff5
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2025-03-31 23:27:56 -0300

    init

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,9 @@
+import type { PageServerLoad } from './$types';
+import { db } from '$lib/server/db';
+import { user } from '$lib/server/db/schema';
+export const load = (async () => {
+	await db.insert(user).values({ age: Math.floor(Math.random() * 100) });
+	return {
+		users: await db.select().from(user)
+	};
+}) satisfies PageServerLoad;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import type { PageData } from './$types';
+
+	let { data }: { data: PageData } = $props();
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+{JSON.stringify(page.data, null, 2)}
